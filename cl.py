4c9e584de90eb968f4d6dc06c30deb05279d9484
AnalystZaid09 <analyst.zaid@gmail.com>
2026-01-10 21:37:03 +0530

    Update cl.py

--- cl.py
+++ cl.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import io
 
-st.set_page_config(page_title="Inventory Analysis Dashboard", layout="wide")
+st.set_page_config(page_title="Stock Movement Analysis Dashboard", layout="wide")
 
-st.title("📊 Inventory Analysis Dashboard")
+st.title("📊 Stock Movement Analysis Dashboard")
 
 # File uploaders
 st.sidebar.header("Upload Files")
